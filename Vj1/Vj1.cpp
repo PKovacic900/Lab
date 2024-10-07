831948bdf78e4caf6e40d26f38beccf32bd3c911
Lbraica <amrkon01@fesb.hr>
2024-10-07 18:35:10 +0200

    first task

--- Vj1/Vj1.cpp
+++ Vj1/Vj1.cpp
@@ -1,9 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#define MAX 1024
 
+int ReadRows(const char* fileName);
 
-// Dodan komentar
 int main()
 {
-	printf("Hello world\n");
+	int numberOfRow =0; 
+	
+	numberOfRow = ReadRows("dat.txt");
 	return 0;
+}
+
+int ReadRows(const char* fileName)
+{
+    int numberOfRow = 0;
+    FILE*fp = NULL;
+    char buffer[MAX] = {'\0'};
+    
+    
+    
+    fp = fopen(fileName, "r");
+    
+    if(!fp)
+    {
+           return-1;
+    }
+    
+    while(!feof(fp))
+    {
+                    fgets(buffer, MAX, fp);
+                    numbere
+    }
 }
